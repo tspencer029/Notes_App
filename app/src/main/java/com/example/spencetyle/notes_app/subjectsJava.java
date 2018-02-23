86bd96f2b26d42167aa2b195e31691452b862d8f
MILLSMA <36748411+MILLSMA@users.noreply.github.com>
2018-02-24 10:17:55 +1300

    buttons?

--- app/src/main/java/com/example/spencetyle/notes_app/subjectsJava.java
+++ app/src/main/java/com/example/spencetyle/notes_app/subjectsJava.java
@@ -2,12 +2,16 @@ package com.example.spencetyle.notes_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class subjectsJava extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.subjects);
+    }
+
+    public void onClick(View view) {
     }
 }
